f867e4ecf13697bd0a8ba4e50fecc9f71d994fec
Cantillo <cantillohenao@gmail.com>
2021-10-24 22:33:09 -0500

    Crea la clase categoria para definir el tipo de dificultad de cada pregunta

---
+++ src/com/company/modelos/Categoria.java
@@ -0,0 +1,17 @@
+package com.company.modelos;
+
+public class Categoria {
+    private String tipo;
+
+    public Categoria(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+}
